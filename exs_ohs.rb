7963dfb15e83fa1d27f4da4d660db6074935773c
Kyle Shive <shivekyle@gmail.com>
2018-04-17 14:09:53 -0500

    Initial attempt

---
+++ exs_ohs.rb
@@ -0,0 +1,10 @@
+# Codewars #
+
+#Check to see if a string has the same amount of 'x's and 'o's. 
+#The method must return a boolean and be case insensitive. 
+#The string can contain any char.
+
+def XO (str)
+	str.downcase!
+	str.count('o') == str.count('x')
+end
