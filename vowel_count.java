a0bc37f08f1e1ff54d0fc2a250e48bd44dc6804f
Kyle Shive <shivekyle@gmail.com>
2018-04-30 08:51:26 -0500

    initial project version

---
+++ vowel_count.java
@@ -0,0 +1,19 @@
+public class Vowels {
+
+  public static int getCount(String str) {
+    int vowelsCount = 0;
+    
+    char[] charArray = str.toCharArray();
+    for (int i = 0; i <= charArray.length -1; i++) {
+        if (charArray[i] == 'a' ||
+            charArray[i] == 'e' ||
+            charArray[i] == 'i' ||
+            charArray[i] == 'o' ||
+            charArray[i] == 'u') {
+            vowelsCount++;
+            }
+    
+    }
+    
+    return vowelsCount;
+  }
